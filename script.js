b1af64dfe67595102603d4f8dd73769b9e62d8c5
Aisling Carroll <aislingcarroll0801@gmail.com>
2024-07-26 16:30:53 +0100

    Added ability to add one item and name bag. Changed some simple styling temporarily for clarity

--- script.js
+++ script.js
@@ -6,7 +6,31 @@ function intialise() {
 }
 
 function addBag() {
+    let bagContainer = document.getElementById('bag-container');
     let newBag = document.createElement('div');
     newBag.id = 'bag'
-    document.getElementById('bag-container').appendChild(newBag);
+
+    let bagName = nameBag();
+    let bagContents = addItem(); 
+
+    newBag.appendChild(bagName);
+    newBag.appendChild(bagContents);
+
+    bagContainer.appendChild(newBag);
+}
+
+function nameBag() {
+    let bagName = document.createElement('input');
+    bagName.type = 'text';
+    bagName.placeholder = 'Name your bag';
+    bagName.id = 'bag-name'
+    return bagName;
+}
+
+function addItem() {
+    let bagContents = document.createElement('input');
+    bagContents.type = 'text';
+    bagContents.placeholder = 'Add new item'
+    bagContents.id = 'bag-contents'
+    return bagContents;
 }
